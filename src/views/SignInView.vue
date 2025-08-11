bf80efdbaa4b6de90937a16e34eaee48623b3e4c
clarencelinwork <clarencelinwork@gmail.com>
2025-08-11 22:38:01 +0800

    [feature] sign in finished

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -4,9 +4,48 @@ import SideComponent from '@/components/Side.vue'
 import EmailInput from '@/components/EmailInput.vue'
 import PasswordInput from '@/components/PasswordInput.vue'
 
-function showTest() {
-  console.log('test')
+import { ref } from 'vue'
+import axios from 'axios'
+import Cookies from 'js-cookie'
+
+const email = ref('')
+const password = ref('')
+const errorMessage = ref('')
+
+function submitForm() {
+  const site = 'https://todolist-api.hexschool.io'
+
+  const requestUrl = `${site}/users/sign_in`
+  axios
+    .post(requestUrl, {
+      email: email.value,
+      password: password.value,
+    })
+    .then((response) => {
+      Cookies.set('token', response.data.token, { expires: 7 })
+      Cookies.set('tokenExpired', response.data.exp, { expires: 7 })
+      errorMessage.value = "登入成功"
+    })
+    .catch((error) => {
+      console.log(error)
+      if (Array.isArray(error.response.data.message)) {
+        // 如果是陣列，取得第一個元素
+        errorMessage.value = error.response.data.message[0]
+      } else {
+        // 如果不是陣列（例如是字串），就直接使用
+        errorMessage.value = error.response.data.message
+      }
+    })
 }
+
+function getEmailInput(value) {
+  email.value = value
+}
+
+function getPasswordInput(value) {
+  password.value = value
+}
+
 </script>
 
 <template>
@@ -17,14 +56,10 @@ function showTest() {
       <div>
         <form class="formControls" action="index.html">
           <h2 class="formControls_txt">最實用的線上代辦事項服務</h2>
-          <EmailInput />
-          <PasswordInput
-            input-name="password"
-            input-id="password"
-            label-name="密碼"
-            placeholder="請輸入密碼"
-          />
-          <input class="formControls_btnSubmit" type="button" v-on:click="showTest" value="登入" />
+          <EmailInput @update:value="getEmailInput" />
+          <PasswordInput input-name="password" input-id="password" label-name="密碼" placeholder="請輸入密碼" @update:value="getPasswordInput"/>
+          <div id="errorMessage">{{ errorMessage }}</div>
+          <button class="formControls_btnSubmit" type="button" v-on:click="submitForm">登入</button>
           <RouterLink class="formControls_btnLink" to="/sign-up">註冊帳號</RouterLink>
         </form>
       </div>
@@ -32,4 +67,8 @@ function showTest() {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#errorMessage {
+  color: red;
+}
+</style>
