250ac9aa08b774d1ebea21c68b137ae850ea1c02
Clarence <clarencelinwork@gmail.com>
2025-08-12 16:05:32 +0800

    [chore] Updates code formatting and style

--- src/views/TodoListView.vue
+++ src/views/TodoListView.vue
@@ -61,27 +61,27 @@ function changeType(type) {
     default:
     case '全部':
       currentType.value = '全部'
-      break;
+      break
     case '待完成':
       currentType.value = '待完成'
-      break;
+      break
     case '已完成':
       currentType.value = '已完成'
-      break;
+      break
   }
   checkCount()
 }
 
 function _uuid() {
-  let d = Date.now();
-  if (typeof performance !== 'undefined' && typeof performance.now === 'function'){
-    d += performance.now(); //use high-precision timer if available
+  let d = Date.now()
+  if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+    d += performance.now() //use high-precision timer if available
   }
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
-    const r = (d + Math.random() * 16) % 16 | 0;
-    d = Math.floor(d / 16);
-    return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-  });
+    const r = (d + Math.random() * 16) % 16 | 0
+    d = Math.floor(d / 16)
+    return (c === 'x' ? r : (r & 0x3) | 0x8).toString(16)
+  })
 }
 </script>
 
