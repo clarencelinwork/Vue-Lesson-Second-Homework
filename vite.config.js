cdbade9bf69a3b9085f5aef0116c27fe42f68e52
clarencelinwork <clarencelinwork@gmail.com>
2025-08-13 15:52:25 +0800

    [feature] Sets the base URL for the application

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Vue-Lesson-Second-Homework/',
   plugins: [
     vue(),
     vueDevTools(),
